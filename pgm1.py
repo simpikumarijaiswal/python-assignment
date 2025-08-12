a7d0716ca53a4beb95af5d39cfdb8a2cde08b2d8
simpi kumari <37670915+simpikumarijaiswal@users.noreply.github.com>
2025-08-13 03:13:51 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number: "))
+b=int(input("enter another number: "))
+c=a+b
+d=a-b
+e=a*b
+print("addition of two number is",c)
+print("subtraction of two number is",d)
+print("multiplication of two number is",e)
+print("division of two number is",e)
---
+++ pgm2.py
@@ -0,0 +1,4 @@
+str1=input("enter first name:  ")
+str2=input("enter second name:  ")
+str3=str1+str2
+print("welcome to the python word ",str3)
